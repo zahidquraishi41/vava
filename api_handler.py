a6475989f0853be8ea0405732569a46db51dccf7
zahid <zahidquraishi41@gmail.com>
2025-09-30 15:30:10 +0530

    api docs bug fix

--- api_handler.py
+++ api_handler.py
@@ -22,70 +22,73 @@ class Wrapper:
 class APIHandler:
     def __init__(self) -> None:
         self._apis = [Wrapper(cls) for cls in apis]
-        self._apis.sort()
 
     def _ping(self, cmd: Command) -> bool:
         if not cmd.is_admin:
-            return 'Admin privilege is required.'
+            return "Admin privilege is required."
 
-        if cmd.param != 'all':
-            return self._apis.index(cmd.param).instance.ping()
+        if cmd.param != "all":
+            if cmd.param not in self._apis:
+                return f"{cmd.param} is not a valid command."
+            i = self._apis.index(cmd.param)
+            return self._apis[i].instance.ping()
+
         outputs = []
         for api in self._apis:
-            outputs.append(f'{api.name} {api.instance.ping()}')
-        return '\n'.join(outputs)
+            outputs.append(f"{api.name} {api.instance.ping()}")
+        return "\n".join(outputs)
 
     def _toggle(self, cmd: Command) -> str:
         if not cmd.is_admin:
-            return 'Admin privilege is required.'
+            return "Admin privilege is required."
 
         if cmd.param in self._apis:
             api = self._apis[self._apis.index(cmd.param)]
-            if cmd.command == 'enable':
+            if cmd.command == "enable":
                 if api.active:
-                    return 'The command is already enabled. No changes were made.'
+                    return "The command is already enabled. No changes were made."
                 api.active = True
-                return 'The command has been successfully enabled!'
-            elif cmd.command == 'disable':
+                return "The command has been successfully enabled!"
+            elif cmd.command == "disable":
                 if not api.active:
-                    return 'The command is already disabled. No changes were made.'
+                    return "The command is already disabled. No changes were made."
                 api.active = False
-                return 'The command has been successfully disabled.'
+                return "The command has been successfully disabled."
 
-        if cmd.command.startswith('enable'):
-            s = 'List of enabled commands:'
+        if cmd.command.startswith("enable"):
+            s = "List of enabled commands:"
             enabled = [api.name for api in self._apis if api.active]
-            return s + '\n' + '\n'.join(enabled)
+            return s + "\n" + "\n".join(enabled)
 
         else:
-            s = 'List of enabled commands:'
+            s = "List of enabled commands:"
             disabled = [api.name for api in self._apis if not api.active]
-            return s + '\n' + '\n'.join(disabled)
+            return s + "\n" + "\n".join(disabled)
 
     def _help(self, cmd: Command) -> str:
+        names = [api.name for api in self._apis]
         if not cmd.param:
-            s = 'List of commands:'
-            names = [api.name for api in self._apis]
+            s = "List of commands:"
             names.sort()
-            return s + '\n' + '\n'.join(names)
+            return s + "\n" + "\n".join(names)
 
-        if cmd.param not in self._apis:
-            return f'{cmd.param} is not a valid command.'
+        if cmd.param not in names:
+            return f"{cmd.param} is not a valid command."
 
-        i = self._apis.index(cmd.param)
-        doc = apis[i].__doc__ if apis[i].__doc__ else 'No document found.'
-        doc = re.sub(r'(?<=\n) +', '', doc)
+        i = names.index(cmd.param)
+        doc = apis[i].__doc__ or "No document found."
+        doc = re.sub(r"(?<=\n) +", "", doc)
         return doc
 
     def process(self, message) -> str | None:
         cmd = Command(message)
 
         # system commands
-        if cmd.command in ('enable', 'enabled', 'disable', 'disabled'):
+        if cmd.command in ("enable", "enabled", "disable", "disabled"):
             cmd.output = self._toggle(cmd)
-        elif cmd.command == 'help':
+        elif cmd.command == "help":
             cmd.output = self._help(cmd)
-        elif cmd.command == 'ping':
+        elif cmd.command == "ping":
             cmd.output = self._ping(cmd)
 
         # api commands
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord as dc
 from api_handler import APIHandler
 import os
 from flask_app import keep_alive
-
+from dotenv import load_dotenv
 
 intents = dc.Intents.default()
 intents.message_content = True
@@ -25,9 +25,11 @@ async def on_message(message: dc.Message):
         out = manager.process(message)
         if out:
             await reply(out)
+    except KeyboardInterrupt:
+        return
     except Exception as e:
         await reply(e)
 
-
+load_dotenv()
 keep_alive()
-client.run(os.environ['VAVA_TOKEN'])
+client.run(os.getenv('VAVA_TOKEN'))
